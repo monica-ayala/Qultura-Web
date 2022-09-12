15dfe5d71dfd4d06e4b78a5ded094fbfe2579797
Jorge Guerrero <a01411752@tec.mx>
2022-09-12 18:37:14 -0500

    Selects funcionando

--- public/js/script_update_user.js
+++ public/js/script_update_user.js
@@ -1,25 +1,33 @@
 $(document).ready(function(){
-    $('.modal').modal();
+    $('.formSelect').formSelect();
+    $('select').formSelect();
+    $('.modal').modal({
+        onOpenStart: function(){
+            //str = $("#id_user").text();
+        },
+        onCloseEnd: function(){
+            $('.formSelect').empty();
+            $('select').empty();
+        }
+    });
   });
 
-  $('.formSelect').formSelect();
-  $('select').formSelect();
   $('.formSelect').on('contentChanged', function() {
     $(this).formSelect();
   });  
 
+
 function getInfoUser(){
     jQuery('#modal1').modal('open');
     
-    console.log("holi")
-
-    let modal = document.getElementById("pruebita");
-    let selectrol=document.getElementById("mySelect");
+    let magia=document.getElementById("boton").value;
+    let selectrol=document.getElementById("sl_roles");
+    let selectmuseos=document.getElementById("sl_museos");
     let user_id = document.getElementById("id_user").value;
 
     let ruta ="/usuario/rol/"+ user_id;
 
-    console.log(modal, user_id);
+    console.log( magia,user_id);
 
     fetch(ruta,{
         method: 'GET',
@@ -30,27 +38,26 @@ function getInfoUser(){
     .then(response => response.json())
     .then(response => {
             console.log(response);
-            
-           // modal.innerHTML+='<div class="input-field">'
-           // modal.innerHTML+='<select id="mySelect">'
             selectrol.innerHTML+='<option value="" disabled selected> Choose your option</option>'
             for (let rol of response.roles){
                 //var $newOpt = $("<option>").attr("value",rol.id_rol).text(rol.nom_rol)
                 //$("#mySelect").append($newOpt);
                 selectrol.innerHTML+='<option value='+rol.id_rol+'>'+rol.nom_rol+'</option>'
             }
-            $("#mySelect").trigger('contentChanged');
             $('select').formSelect();
-            selectrol.innerHTML+='</select> <label>Rol</label>'
-            //$('select').formSelect();
-            //selectrol.innerHTML+='</div>'
-        
-          // selectrol.innerHTML+='</div>'
-        
-           //selectrol.innerHTML+='</div>'
-          //  modal.innerHTML+='</div>'
-           // modal.innerHTML+='</div>'
-        
+            $("#sl_roles").trigger('contentChanged');
+
+            selectmuseos.innerHTML+='<option value="" disabled selected> Choose your option</option>'
+            for (let museo of response.museos){
+                //var $newOpt = $("<option>").attr("value",rol.id_rol).text(rol.nom_rol)
+                //$("#mySelect").append($newOpt);
+                selectmuseos.innerHTML+='<option value='+museo.id_museo+'>'+museo.nom_museo+'</option>'
+            }
+            $('select').formSelect();
+            $("#sl_museos").trigger('contentChanged');
+
+            
+            
             
             
             
