1a53394ac181bdcc3bce903ffcca4f96c3da11e3
unknown <a01707439@itesm.mx>
2022-09-20 20:54:48 -0500

    corrected multer

--- util/database.js
+++ util/database.js
@@ -28,6 +28,6 @@ const DB_LOCAL = {
 
 //const pool = mysql.createPool(DB_LOCAL);
 //const pool = mysql.createPool(DB_INTERLINK);
-const pool = mysql.createPool(DB_AWS);
+const pool = mysql.createPool(DB_INTERLINK);
 
 module.exports = pool.promise();
