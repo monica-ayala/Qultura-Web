75215352d0911829dc6cbd937f1ab9d876157c2d
unknown <a01707439@itesm.mx>
2022-09-21 16:58:17 -0500

    push api

--- routes/museo_routes.js
+++ routes/museo_routes.js
@@ -5,7 +5,8 @@ const path = require('path');
 // const isAuth= require ('../util/is-auth.js')
 const museo_controller = require('../controllers/museo_controller');
 router.get('/',museo_controller.lista);
-router.get('/get',museo_controller.get_museo);
+router.get('/get',museo_controller.get_museo_api);
+router.get('/getAll',museo_controller.get_all_api);
 router.get('/nuevo_museo',museo_controller.get_nuevo)
 router.post('/nuevo_museo',museo_controller.museo_post)
 router.post('/borrar/:id_museo',museo_controller.soft_erase)
