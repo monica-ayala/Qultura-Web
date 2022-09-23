4eb1717490e78dd2f5aabb98d9217d1c66b60fd6
mon <a01707439@itesm.mx>
2022-09-23 09:47:20 -0500

    fix

--- routes/evento_routes.js
+++ routes/evento_routes.js
@@ -6,21 +6,6 @@ const path = require('path');
 
 
 const event_controller = require('../controllers/evento_controller');
-<<<<<<< HEAD
-
-//router.get('/',museo_controller.lista);
-//router.get('/get',museo_controller.get_museo);
-
-router.get('/nuevo_evento',event_controller.get_nuevo)
-router.post('/nuevo_evento',event_controller.post_nuevo)
-router.post('/borrar/evento/:id_evento',event_controller.erase)
-
-
-//router.post('/regresar/:id_museo',museo_controller.soft_unerase)
-//router.get('/registro',museo_controller.register)
-//router.get('/:id_museo',museo_controller.get_Onemuseo)
-=======
->>>>>>> module2
 
 //router.get('/',museo_controller.lista);
 //router.get('/get',museo_controller.get_museo);
