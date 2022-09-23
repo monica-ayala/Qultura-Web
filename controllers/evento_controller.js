49ad4f2a74f16da7fb4ab72d03ed80bbace27f8a
Jorge Guerrero <a01411752@tec.mx>
2022-09-22 19:13:12 -0500

    Evento

--- controllers/evento_controller.js
+++ controllers/evento_controller.js
@@ -7,7 +7,6 @@ const Evento = require("../models/evento");
 exports.get_nuevo=(request,response,next)=>{
   Evento.fetchTags()
   .then(([rowsTags])=>{
-    console.log(rowsTags);
     response.render('nuevo_evento',{
       tags:rowsTags
     });
@@ -16,7 +15,7 @@ exports.get_nuevo=(request,response,next)=>{
 };
 
 exports.post_nuevo = (request, response, next) => {
-  console.log(request.body.seventos);
+  console.log(request.body.sevento);
     url_imagen = request.file;
     if((typeof(url_imagen) == "undefined")){
         url_imagen = "";
@@ -24,7 +23,7 @@ exports.post_nuevo = (request, response, next) => {
         url_imagen = request.file.filename;
     }
     url_imagenB = request.file;
-    //console.log(request)
+    
     link_ubi="placeholder";
     const nuevo_evento = new Evento(
       request.body.info_evento,
@@ -34,6 +33,9 @@ exports.post_nuevo = (request, response, next) => {
     )
     nuevo_evento.save()
     .then((result) => {
+        for(let tag of request.body.sevento){
+          Evento.AsignTags(tag,result[0].insertId);
+        };
       response.redirect ("/museo");
     }).catch(err => console.log(err));  
   };
--- models/evento.js
+++ models/evento.js
@@ -24,7 +24,12 @@ module.exports = class Evento{
     }
 
     static softErase(id_evento){
-        return db.execute('DELETE FROM Evento WHERE id_evento = ?' , [id_evento])
+        db.execute('DELETE FROM Evento_Tag WHERE id_evento_tag=?',[id_evento])
+        .then(([rows,fieldData])=>{
+            return db.execute('DELETE FROM Evento WHERE id_evento = ?' , [id_evento])
+        })
+        .catch((err=>console.log(err)))
+        
     }
     static fecthOne(id_evento) {
         return db.execute('SELECT * FROM Evento WHERE id_evento=?',
@@ -33,6 +38,10 @@ module.exports = class Evento{
 
     static fetchTags(){
         return db.execute('SELECT * FROM Tag');
+    }
+
+    static AsignTags(tags,ide_evento){
+        return db.execute('INSERT INTO Evento_Tag (id_evento_tag, id_tag_evento) VALUES (?,?)',[ide_evento,tags])
     }
 
 }
