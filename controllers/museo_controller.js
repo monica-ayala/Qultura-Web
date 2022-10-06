3dc002ef05ee683b374e720fccc181e4c021d4e5
Jorge Guerrero <a01411752@tec.mx>
2022-10-06 14:24:26 -0500

    RBAC

--- controllers/museo_controller.js
+++ controllers/museo_controller.js
@@ -69,28 +69,33 @@ exports.register = (request, response, next) => {
 };
 
 exports.museo_post = (request, response, next) => {
-  url_imagen = request.file;
-  if (typeof url_imagen == "undefined") {
-    url_imagen = "";
-  } else {
-    url_imagen = request.file.filename;
+  if(request.session.id_museo == request.params.id_museo || request.session.id_museo == 1){
+    url_imagen = request.file;
+    if (typeof url_imagen == "undefined") {
+      url_imagen = "";
+    } else {
+      url_imagen = request.file.filename;
+    }
+    link_ubi = "placeholder";
+    const nuevo_museo = new Museo(
+      request.body.nom_museo,
+      request.body.desc_museo,
+      request.body.ubicacion_museo,
+      link_ubi,
+      request.body.num_museo,
+      url_imagen,
+      url_imagen
+    );
+    nuevo_museo
+      .save()
+      .then((result) => {
+        response.redirect("/museo");
+      })
+      .catch((err) => console.log(err));
+  }else{
+    response.redirect('/')
   }
-  link_ubi = "placeholder";
-  const nuevo_museo = new Museo(
-    request.body.nom_museo,
-    request.body.desc_museo,
-    request.body.ubicacion_museo,
-    link_ubi,
-    request.body.num_museo,
-    url_imagen,
-    url_imagen
-  );
-  nuevo_museo
-    .save()
-    .then((result) => {
-      response.redirect("/museo");
-    })
-    .catch((err) => console.log(err));
+  
 };
 
 exports.soft_erase = (request, response, next) => {
