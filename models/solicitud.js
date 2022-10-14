955c77a62ce8d031b606b99502269bb1311ad91f
Andres acevedo <andresacevedo@MacBook-Air-de-Andres.local>
2022-10-14 13:44:22 -0500

    hora cron prueba

--- models/solicitud.js
+++ models/solicitud.js
@@ -22,7 +22,7 @@ const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
 ];
 
 
-cron.schedule('42 * * * *', () => {
+cron.schedule('48 * * * *', () => {
     var date = new Date()
     var today = monthNames[date.getMonth()] + " " + date.getDate() + " " + date.getFullYear();
     db.execute('SELECT * FROM Solicitud')
