6b93f9f290fad7c31ecf9714bfe4965ca0af8503
mon <a01707439@itesm.mx>
2022-10-14 16:02:44 -0500

    token implementation

---
+++ util/is-valid.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const jwt = require("jsonwebtoken")
+
+module.exports=(request,response,next) =>{
+    const authHeader = request.session.auth
+    console.log(request.session.auth)
+    if (authHeader) {
+        const token = authHeader;
+        console.log(token)
+        jwt.verify(token, process.env.TOKEN_SECRET, (err, usuario) => {
+            if (err) {
+                return response.sendStatus(403);
+            }
+            next();
+        });
+    } else {
+        response.sendStatus(401);
+    }
+}
