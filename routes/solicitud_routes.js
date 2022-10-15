e56c37be40f1ff727149b2e15e8adf160620fd5d
EmilianoV-TEC <a01707035@itesm.mx>
2022-10-15 10:30:13 -0800

    Agrega token a rutas

--- routes/solicitud_routes.js
+++ routes/solicitud_routes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const solicitud_controller = require('../controllers/solicitud_controller');
+const api= require ('../util/is-valid-api.js')
 
 router.get('/getAll/:id_usuario', solicitud_controller.get_solicitudes);
 
-router.post('/cancelar', solicitud_controller.elimina_solicitud);
+router.post('/cancelar', api, solicitud_controller.elimina_solicitud);
 
-router.post('/nuevaSolicitud', solicitud_controller.agrega_solicitud);
+router.post('/nuevaSolicitud', api, solicitud_controller.agrega_solicitud);
 
 router.get('/aceptar/:id_status', solicitud_controller.updateAceptar_solicitud);
 
