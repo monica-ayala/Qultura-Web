d9048adcb1610a68436f478c929a4bf67e072e40
mon <a01707439@itesm.mx>
2022-10-17 02:39:56 -0500

    erased logs

--- util/is-valid.js
+++ util/is-valid.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken")
 
 module.exports=(request,response,next) =>{
     const authHeader = request.session.auth
-    console.log(request.session.auth)
     if (authHeader) {
         const token = authHeader;
         jwt.verify(token, process.env.TOKEN_SECRET, (err, usuario) => {
