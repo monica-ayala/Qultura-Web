a1a7848ecb00e8fac4f7e1c0cfd0ef0bb977d016
Caceres-A01706972 <A01706972@tec.mx>
2022-10-20 18:47:37 -0500

    Adds is-img2

--- routes/guia_routes.js
+++ routes/guia_routes.js
@@ -4,13 +4,14 @@ const isAuth= require ('../util/is-auth.js')
 const isImage = require('../util/is-img.js')
 
 const guia_controller = require('../controllers/guia_controller');
+const isImg2 = require('../util/is-img2.js');
 
 // Guias controller calls
 router.get('/',isAuth,guia_controller.view);
 router.get('/editar_guia/:id_guia',isAuth,guia_controller.get_editarGuia);
-router.post('/editar_guia',isImage, isAuth,guia_controller.post_editarGuia);
-router.get('/agregar_guia',isImage, isAuth,guia_controller.get_agregarGuia);
-router.post('/agregar_guia',isImage, isAuth,guia_controller.post_agregarGuia);
+router.post('/editar_guia', isAuth, isImg2,guia_controller.post_editarGuia);
+router.get('/agregar_guia',isAuth, isImg2,guia_controller.get_agregarGuia);
+router.post('/agregar_guia',isAuth, isImg2,guia_controller.post_agregarGuia);
 router.get('/eliminar_guia/:id_guia',isAuth,guia_controller.eliminarGuia)
 
 // REST API
