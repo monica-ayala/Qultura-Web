554e7b9bc87b438153eab58ed3e4cbc03c5c1492
Jorge Guerrero <a01411752@tec.mx>
2022-10-20 10:58:26 -0500

    Imagenes evento

--- routes/evento_routes.js
+++ routes/evento_routes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const filesystem = require('fs');
 const path = require('path');
 const isAuth= require ('../util/is-auth.js')
+const isImg = require('../util/is-img.js')
 const event_controller = require('../controllers/evento_controller');
 
 // Evento controller calls
 router.get('/nuevo_evento',isAuth,event_controller.get_nuevo)
-router.post('/nuevo_evento',isAuth,event_controller.post_nuevo)
+router.post('/nuevo_evento',isAuth,isImg,event_controller.post_nuevo)
 router.post('/borrar/evento/:id_evento',isAuth,event_controller.erase)
 
 // REST API
